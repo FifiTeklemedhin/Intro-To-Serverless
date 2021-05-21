b4b46d0e24d2d5c3e64cfcfbd9d37a887fde9fbb
fifi-teklemedhin <fteklemedhin@utah.gov>
2021-05-20 21:30:07 -0600

    updated week 1

--- .history/HackerVoice/index_20210520212027.js
+++ .history/HackerVoice/index_20210520212027.js
@@ -3,9 +3,9 @@ module.exports = async function (context, req) {
 
     const password = req.query.password;
     //const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = password
-        ? password 
-        : "Put in a password!";
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
