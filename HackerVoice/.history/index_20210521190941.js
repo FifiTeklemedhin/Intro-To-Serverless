73267e359df8143b0135074b0b3c394b01b1d214
fifi-teklemedhin <fteklemedhin@utah.gov>
2021-05-21 19:45:59 -0600

    updated week 1 steps 5-78

---
+++ HackerVoice/.history/index_20210521190941.js
@@ -0,0 +1,13 @@
+require('node-fetch')
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    //const name = (req.query.name || (req.body && req.body.name));
+    let responseMessage = "";
+    base64data = Buffer.from(originaldata).toString('base64')
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
---
+++ HackerVoice/.history/index_20210521191931.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    //const name = (req.query.name || (req.body && req.body.name));
+    let resp1 = await fetch("https://cataas.com/cat/cute/says/Daniel", {
+        method: 'GET'
+    });
+    let data1 = await resp.arrayBuffer()
+    base64data1 = Buffer.from(data1).toString('base64')
+
+    let resp2 = await fetch("https://cataas.com/cat/cute/says/Shreya", {
+        method: 'GET'
+    });
+    let data2 = await resp.arrayBuffer()
+    base64data2 = Buffer.from(data2).toString('base64')
+
+    let responseMessage = {
+        "cat1": base64data1,
+        "cat2": base64data2,
+        "names": [
+            "Daniel",
+            "Shreya"
+    }
+    // we need to receive it as a buffer since this is an image we are receiving from the API
+    // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: base64data
+    };
+}
