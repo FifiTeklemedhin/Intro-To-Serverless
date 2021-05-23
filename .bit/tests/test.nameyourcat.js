f984e93ab93cd7a25501f64dc74762fb0d669a00
fifi-teklemedhin <fteklemedhin@utah.gov>
2021-05-23 12:46:54 -0600

    updated week 1 steps 6 and 7 tests

--- .bit/tests/test.nameyourcat.js
+++ .bit/tests/test.nameyourcat.js
@@ -28,6 +28,21 @@ try {
             throw new Error("Sorry! We couldn't find one or both of the cat pictures. Make sure you encoded in BASE64!")
         }
 
+        var array = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+        var names = data.names
+        var name1 = names[0]
+        var name2 = names[1]
+
+        try {
+            if (array.includes(name1) && array.includes(name2)) {
+                console.log(`Yay! 🎉 Thanks for getting our names right. We got: ${name1} and ${name2}`)
+            } else {
+                throw new Error(`Sorry, your names, ${name1} and ${name2}, were not correct.`)
+            }
+        } catch(e) {
+            throw new Error(`Sorry, your names, ${name1} and ${name2}, were not correct.`)  
+        }
+
     })().catch( e => { console.error(e) })
 } catch (e) {
     throw new Error("You have not added your function url as a secret!");
